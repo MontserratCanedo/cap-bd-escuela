a0354bcf659fc3e9e4e9ddfd9476c3fcb21fb71f
Joahan3250 <joahan.sanchez.aloga@outlook.com>
2024-09-18 15:04:58 -0600

    calificaciones

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-const userService = require('./services/alumnosService'); //guardamos en una variable, los métodos exportados en alumnosServices
-const db = require('./utils/conexion'); //guardamos en una variable, la conexion a la BD
-
-
+const calificacionService = require("./services/calificacionesService"); //guardamos en una variable, los métodos exportados en alumnosServices
+const db = require("./utils/conexion"); //guardamos en una variable, la conexion a la BD
 
 // Ejemplos de uso
 const runExamples = async () => {
   try {
-    // // Crear un nuevo usuario
-    // const newUserId = await createUser('John Doe', 'john.doe@example.com');
-    // console.log('Created user with ID:', newUserId);
+    // // Crear un nuevo
+    const newCalificacionId = await calificacionService.createCalificacion(
+      "13.0"
+    );
+    console.log("Calificaciones creadas con el ID: ", newCalificacionId);
 
-    // Obtener todos los usuarios
-    const users = await userService.getAllClients();
-    console.log('All users:', users);
+    // Obtener todo
+    const calificaciones = await calificacionService.getAllCalificaciones();
+    console.log("Todas las calificaciones: ", calificaciones);
 
-    // // Obtener un usuario por ID
-    // const user = await getUserById(newUserId);
-    // console.log('User with ID', newUserId, ':', user);
+    // Obtener todos los por ID
+    const calificaciones2 = await calificacionService.getCalificacionById(196);
+    console.log("Calificaciones con el ID", ":", calificaciones2);
 
     // // Actualizar un usuario
-    // await updateUser(newUserId, 'John Smith', 'john.smith@example.com');
-    // console.log('Updated user with ID:', newUserId);
-
-    // // Obtener el usuario actualizado
-    // const updatedUser = await getUserById(newUserId);
-    // console.log('Updated user with ID', newUserId, ':', updatedUser);
+    await calificacionService.updateCalificacion(newCalificacionId, "8.0");
+    console.log("Calificaciones actualizadas con el ID:", newCalificacionId);
 
     // // Eliminar un usuario
-    // await deleteUser(newUserId);
-    // console.log('Deleted user with ID:', newUserId);
-
-    // // Verificar si el usuario fue eliminado
-    // const deletedUser = await getUserById(newUserId);
-    // console.log('User with ID', newUserId, 'after deletion:', deletedUser);
+    await calificacionService.deletedCalificacion(newCalificacionId);
+    console.log("Calificaciones eliminadas con el ID:", newCalificacionId);
+    const deletedCalificacion = await calificacionService.getCalificacionById(
+      newCalificacionId
+    );
+    console.log(
+      "User with ID",
+      newCalificacionId,
+      "after deletion:",
+      deletedCalificacion
+    );
   } catch (error) {
-    console.error('Error:', error.message); //captura el error y lo muestra
+    console.error("Error:", error.message);
   } finally {
-    db.end(); // Cierra la conexión a la base de datos
+    db.end();
   }
 };
 
