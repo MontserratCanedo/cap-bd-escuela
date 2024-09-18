0f6e9858bfda28cbed2938de32b1c19c2450896e
die90-ctrl <diego.garcia.aloga@outlook.com>
2024-09-18 14:30:23 -0600

    CRUD maestros

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const userService = require('./services/alumnosService'); //guardamos en una variable, los métodos exportados en alumnosServices
+const maestrosService = require('./services/maestrosService.js'); //guardamos en una variable, los métodos exportados en alumnosServices
 const db = require('./utils/conexion'); //guardamos en una variable, la conexion a la BD
 
 
@@ -9,18 +9,29 @@ const runExamples = async () => {
     // // Crear un nuevo usuario
     // const newUserId = await createUser('John Doe', 'john.doe@example.com');
     // console.log('Created user with ID:', newUserId);
+    /* const newMaestro = await maestrosService.createMaestro('John', 'Doe', 1, 1);
+    console.log('Create maestro:', newMaestro); */
+    
 
     // Obtener todos los usuarios
-    const users = await userService.getAllClients();
-    console.log('All users:', users);
-
-    // // Obtener un usuario por ID
+    /* const users = await userService.getAllClients();
+    console.log('All users:', users); */
+    /* const maestros = await maestrosService.getAllMaestros();
+    console.log('All maestros', maestros); */
+    
+    // Obtener un usuario por ID
     // const user = await getUserById(newUserId);
     // console.log('User with ID', newUserId, ':', user);
+    /* const maestrosByID = await maestrosService.getMaestrosById(1);
+    console.log('Maestro con el ID', maestrosByID ); */
+    
 
     // // Actualizar un usuario
     // await updateUser(newUserId, 'John Smith', 'john.smith@example.com');
     // console.log('Updated user with ID:', newUserId);
+    /* await maestrosService.updateMaestro(4, 'Leo', 'Kg');
+    console.log('Update user with id', updatedMaestro); */
+    //const updatedMaestro = await maestrosService.updateMaestro(4, "Leo", "Gree");
 
     // // Obtener el usuario actualizado
     // const updatedUser = await getUserById(newUserId);
@@ -29,6 +40,7 @@ const runExamples = async () => {
     // // Eliminar un usuario
     // await deleteUser(newUserId);
     // console.log('Deleted user with ID:', newUserId);
+    //const deletedMaestro = await maestrosService.deleteMaestro(45);
 
     // // Verificar si el usuario fue eliminado
     // const deletedUser = await getUserById(newUserId);
